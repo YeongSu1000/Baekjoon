dae9368d10961ffd59ee23bc076db236d5b5fd15
YeongSu1000 <147939982+YeongSu1000@users.noreply.github.com>
2024-03-17 18:38:27 +0900

    [Silver V] Title: 돌 게임, Time: 128 ms, Memory: 14324 KB -BaekjoonHub

---
+++ 백준/Silver/9655. 돌 게임/돌 게임.java
@@ -0,0 +1,15 @@
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        int n = Integer.parseInt(br.readLine());
+
+        if (n % 2 == 0){
+            bw.write("CY");
+        }else bw.write("SK");
+
+        bw.close();
+    }
+}
