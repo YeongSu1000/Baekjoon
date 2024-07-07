28a0097f64b83bb412fbf679783c91158d8ee57a
YeongSu1000 <147939982+YeongSu1000@users.noreply.github.com>
2024-07-07 20:03:46 +0900

    [Bronze V] Title: 연세대학교, Time: 120 ms, Memory: 14132 KB -BaekjoonHub

---
+++ 백준/Bronze/15680. 연세대학교/연세대학교.java
@@ -0,0 +1,18 @@
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        int n = Integer.parseInt(br.readLine());
+
+        if(n == 0){
+            bw.write("YONSEI\n");
+        }else{
+            bw.write("Leading the Way to the Future\n");
+        }
+
+        bw.close();
+    }
+}
