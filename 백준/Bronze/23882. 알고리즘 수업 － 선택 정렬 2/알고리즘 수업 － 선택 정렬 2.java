fd888f6a79e7007a4c0543e7175b22d9d6984468
YeongSu1000 <147939982+YeongSu1000@users.noreply.github.com>
2024-07-18 18:50:09 +0900

    [Bronze I] Title: 알고리즘 수업 - 선택 정렬 2, Time: 484 ms, Memory: 23180 KB -BaekjoonHub

---
+++ 백준/Bronze/23882. 알고리즘 수업 － 선택 정렬 2/알고리즘 수업 － 선택 정렬 2.java
@@ -0,0 +1,48 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int size = Integer.parseInt(st.nextToken());
+        int k = Integer.parseInt(st.nextToken());
+        int[] arr = new int[size];
+        st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < size; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+
+        int count = 0;
+        int last = size - 1;
+        int t = -1;
+        while (last > 0) {
+            int max = 0;
+            int index = 0;
+            for (int i = 0; i <= last; i++) {
+                if (arr[i] > max) {
+                    max = arr[i];
+                    index = i;
+                }
+            }
+            if (max != arr[last]) {
+                arr[index] = arr[last];
+                arr[last] = max;
+                count++;
+            }
+            if (count == k) {
+                for (int i = 0; i < size; i++) {
+                    bw.write(arr[i] + " ");
+                }
+                t = 0;
+            }
+            last--;
+        }
+        if(t == -1){
+            bw.write(t+"");
+        }
+        bw.close();
+    }
+}
