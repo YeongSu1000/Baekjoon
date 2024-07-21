f9291ab872ad19112b45e92de853b33dc52e8cb8
YeongSu1000 <147939982+YeongSu1000@users.noreply.github.com>
2024-07-21 19:26:53 +0900

    [Silver III] Title: 합 구하기, Time: 556 ms, Memory: 51780 KB -BaekjoonHub

---
+++ 백준/Silver/11441. 합 구하기/합 구하기.java
@@ -0,0 +1,32 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        int n = Integer.parseInt(br.readLine());
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int[] arr = new int[n];
+        arr[0] = Integer.parseInt(st.nextToken());
+        for (int i = 1; i < n; i++) {
+            arr[i] = arr[i - 1] + Integer.parseInt(st.nextToken());
+        }
+        int m = Integer.parseInt(br.readLine());
+        int start;
+        int end;
+        for (int i = 0; i < m; i++) {
+            st = new StringTokenizer(br.readLine());
+            start = Integer.parseInt(st.nextToken());
+            end = Integer.parseInt(st.nextToken());
+
+            if (start == 1) {
+                bw.write(arr[end - 1] + "\n");
+            } else {
+                bw.write(arr[end - 1] - arr[start - 2] + "\n");
+            }
+        }
+        bw.close();
+    }
+}
