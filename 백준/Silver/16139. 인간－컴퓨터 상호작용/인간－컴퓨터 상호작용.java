7acc3241ad79166cc176af913248ef6a79bbbbe4
YeongSu1000 <147939982+YeongSu1000@users.noreply.github.com>
2024-07-21 17:54:07 +0900

    [Silver I] Title: 인간-컴퓨터 상호작용, Time: 928 ms, Memory: 116496 KB, Score: 100 point -BaekjoonHub

--- 백준/Silver/16139. 인간－컴퓨터 상호작용/인간－컴퓨터 상호작용.java
+++ 백준/Silver/16139. 인간－컴퓨터 상호작용/인간－컴퓨터 상호작용.java
@@ -7,6 +7,13 @@ public class Main {
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
         String s = br.readLine();
+
+        int[][] arr = new int[26][s.length()];
+        int[] arrSize = new int[26];
+        for (int i = 0; i < arrSize.length; i++) {
+            arrSize[i] = 0;
+        }
+
         int n = Integer.parseInt(br.readLine());
 
         for (int i = 0; i < n; i++) {
@@ -14,26 +21,33 @@ public class Main {
             char a = st.nextToken().charAt(0);
             int start = Integer.parseInt(st.nextToken());
             int end = Integer.parseInt(st.nextToken());
+            // * 문자열 200,000 문제 200,000
 
-            // end까지의 리스트 만들어서 a의 누적합 저장
-            int[] sum = new int[end + 1];
-            if (s.charAt(0) == a) {
-                sum[0] = 1;
-            } else {
-                sum[0] = 0;
+            int index = a - 97;
+
+            if (arrSize[index] == 0) {
+                if (s.charAt(0) == a) {
+                    arr[index][0] = 1;
+                } else {
+                    arr[index][0] = 0;
+                }
+                arrSize[index]++;
             }
-            for (int j = 1; j < sum.length; j++) {
-                sum[j] = sum[j - 1];
-                if (s.charAt(j) == a) {
-                    sum[j]++;
+
+            if (arrSize[index] <= end) { // 1 < 5
+                for (int j = arrSize[index]; j <= end; j++) { // 1 2 3 4
+                    arr[index][j] = arr[index][j - 1];
+                    if (s.charAt(j) == a) {
+                        arr[index][j]++;
+                    }
+                    arrSize[index]++;
                 }
             }
 
-            // sum[end] - sum[start - 1] 구하기
             if (start == 0) {
-                bw.write(sum[end] + "\n");
+                bw.write(arr[index][end] + "\n");
             } else {
-                bw.write(sum[end] - sum[start - 1] + "\n");
+                bw.write(arr[index][end] - arr[index][start - 1] + "\n");
             }
         }
         bw.close();
