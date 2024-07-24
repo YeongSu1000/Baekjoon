2a9c4820f66369376ccb129cf0e7c3a042ab36af
YeongSu1000 <147939982+YeongSu1000@users.noreply.github.com>
2024-07-24 19:14:23 +0900

    [Silver I] Title: 회의실 배정, Time: 484 ms, Memory: 44924 KB -BaekjoonHub

---
+++ 백준/Silver/1931. 회의실 배정/회의실 배정.java
@@ -0,0 +1,51 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringTokenizer st;
+        int n = Integer.parseInt(br.readLine());
+        int[][] arr = new int[n][2];
+
+        for (int i = 0; i < n; i++) {
+            st = new StringTokenizer(br.readLine());
+            arr[i][0] = Integer.parseInt(st.nextToken());
+            arr[i][1] = Integer.parseInt(st.nextToken());
+        }
+        Arrays.sort(arr, new Comparator<int[]>() {
+            @Override
+            public int compare(int[] o1, int[] o2) {
+                if(o1[0] == o2[0])
+                    return o1[1] - o2[1];
+                else
+                    return o1[0] - o2[0];
+            }
+        });
+
+        int start;
+        int end;
+        int i = 0;
+        int count = 0;
+        while (i < arr.length) {
+            start = arr[i][0];
+            end = arr[i][1];
+            i++;
+
+            while (i < arr.length && arr[i][0] < end) { // 시작 시간이 끝나는 시간 보다 작거나 같을때
+                if (arr[i][1] < end) { // 끝나는 시간이 더 작으면
+                    end = arr[i][1];
+                    start = arr[i][0];
+                }
+                i++;
+            }
+            count++;
+            if (i == arr.length) {
+                break;
+            }
+        }
+        bw.write(count + "");
+        bw.close();
+    }
+}
