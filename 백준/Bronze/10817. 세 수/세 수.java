30b5329e3c04081a58d5a5977518a40a571f29ce
YeongSu1000 <147939982+YeongSu1000@users.noreply.github.com>
2024-07-24 16:53:26 +0900

    [Bronze III] Title: 세 수, Time: 132 ms, Memory: 14532 KB -BaekjoonHub

---
+++ 백준/Bronze/10817. 세 수/세 수.java
@@ -0,0 +1,19 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int[] arr = new int[3];
+        arr[0] = Integer.parseInt(st.nextToken());
+        arr[1] = Integer.parseInt(st.nextToken());
+        arr[2] = Integer.parseInt(st.nextToken());
+
+        Arrays.sort(arr);
+
+        bw.write(arr[1] + "");
+        bw.close();
+    }
+}
