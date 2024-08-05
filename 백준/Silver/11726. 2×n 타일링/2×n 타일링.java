b937359639d486f98c5d060a77eecde345b87cdb
YeongSu1000 <147939982+YeongSu1000@users.noreply.github.com>
2024-08-05 17:46:22 +0900

    [Silver III] Title: 2×n 타일링, Time: 100 ms, Memory: 14428 KB -BaekjoonHub

--- 백준/Silver/11726. 2×n 타일링/2×n 타일링.java
+++ 백준/Silver/11726. 2×n 타일링/2×n 타일링.java
@@ -1,21 +1,17 @@
 import java.io.*;
-import java.math.BigInteger;
 
 public class Main {
     public static void main(String[] args) throws IOException {
         input();
         solve();
-        bw.write(ans + "");
+        bw.write(arr[n - 1] + "");
         bw.close();
     }
 
     static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
     static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
     static int n;
-    static int width;
-    static int height;
-    static BigInteger ans = new BigInteger("0");
-    static BigInteger q = new BigInteger("10007");
+    static int[] arr;
 
     static void input() throws IOException {
         n = Integer.parseInt(br.readLine());
@@ -23,26 +19,19 @@ public class Main {
 
     static void solve() {
 
-        width = n / 2;
-        height = n % 2;
+        arr = new int[n];
+        if(n==1){
+            arr[0] = 1;
+        }else if(n==2){
+            arr[0] = 1;
+            arr[1] = 2;
+        }else{
+            arr[0] = 1;
+            arr[1] = 2;
+            for (int i = 2; i < n; i++) {
+                arr[i] = (arr[i-1]%10007 + arr[i-2]%10007) % 10007;
 
-        while (width >= 0) {
-            ans(width, height);
+            }
         }
-    }
-
-    static void ans(int a, int b) {
-        ans = ans.add(factorial(a + b).divide(factorial(a).multiply(factorial(b))));
-        ans = ans.mod(q);
-        width--;
-        height += 2;
-    }
-
-    static BigInteger factorial(int n) {
-        BigInteger r = new BigInteger("1");
-        for (int i = 1; i <= n; i++) {
-            r = r.multiply(new BigInteger(String.valueOf(i)));
-        }
-        return r;
     }
 }
