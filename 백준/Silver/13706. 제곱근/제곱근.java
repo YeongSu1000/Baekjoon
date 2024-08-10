5942e5e57b6e6afc6ce33d91a398dd6b5a9829da
YeongSu1000 <147939982+YeongSu1000@users.noreply.github.com>
2024-08-10 17:42:40 +0900

    [Silver IV] Title: 제곱근, Time: 108 ms, Memory: 14608 KB -BaekjoonHub

--- 백준/Silver/13706. 제곱근/제곱근.java
+++ 백준/Silver/13706. 제곱근/제곱근.java
@@ -2,20 +2,11 @@ import java.io.*;
 import java.math.BigInteger;
 
 public class Main {
-    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-    static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-
     public static void main(String[] args) throws IOException {
-        input();
-        bw.close();
-    }
-
-    static void input() throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
         BigInteger n = new BigInteger(br.readLine());
-        bw.write(test(n) + "");
-    }
-
-    static BigInteger test(BigInteger n) {
-        return n.sqrt();
+        bw.write(n.sqrt() +"");
+        bw.close();
     }
 }
