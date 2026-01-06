4cd7218ea4c919966dbf1234aed21f0202018bb6
YeongSu1000 <147939982+YeongSu1000@users.noreply.github.com>
2026-01-06 22:01:35 +0900

    [Silver IV] Title: 스택, Time: 176 ms, Memory: 19576 KB -BaekjoonHub

--- 백준/Silver/10828. 스택/스택.java
+++ 백준/Silver/10828. 스택/스택.java
@@ -1,63 +1,60 @@
 import java.io.*;
+import java.util.StringTokenizer;
 
 public class Main {
-    static int size = 0;
-
-    public static void push(int x, int[] stack) {
-        stack[size] = x;
-        size++;
-    }
-
-    public static int pop(int[] stack) {
-        int r = -1;
-        if (size == 0) {
-            return r;
-        }else{
-            r = stack[size - 1];
-            stack[size - 1] = 0;
-            size--;
-        }
-        return r;
-    }
-
-    public static int size() {
-        return size;
-    }
-
-    public static int empty() {
-        if(size == 0){
-            return 1;
-        }else return 0;
-    }
-
-    public static int top(int[] stack) {
-        if(size == 0){
-            return -1;
-        }else return stack[size - 1];
-    }
-
     public static void main(String[] args) throws IOException {
+
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        // 명령 갯수 입력
         int n = Integer.parseInt(br.readLine());
+
         int[] stack = new int[n];
+        stack[0] = -1;
+        int top = 0;
+        // 명령 갯수만큼 명령 반복
+        String command;
+
         for (int i = 0; i < n; i++) {
-            String s = br.readLine();
-            if(s.contains("push")){
-                int x = Integer.parseInt(s.substring(s.indexOf(" ")+1));
-                push(x, stack);
-            }else if(s.equals("pop")){
-                bw.write(pop(stack) + "\n");
-            }else if(s.equals("size")){
-                bw.write(size() + "\n");
-            }else if(s.equals("empty")){
-                bw.write(empty() + "\n");
-            }else if(s.equals("top")){
-                bw.write(top(stack) + "\n");
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            command = st.nextToken();
+
+            switch (command) {
+                case "push":
+                    int num = Integer.parseInt(st.nextToken());
+
+                    stack[top] = num;
+                    top++;
+                    break;
+                case "pop":
+                    if (top == 0) {
+                        bw.write(-1 + "\n");
+                    } else {
+                        bw.write(stack[top - 1] + "\n");
+                        top--;
+                    }
+                    break;
+                case "size":
+                    bw.write(top + "\n");
+                    break;
+                case "empty":
+                    if (top == 0) {
+                        bw.write(1 + "\n");
+                    } else {
+                        bw.write(0 + "\n");
+                    }
+                    break;
+                case "top":
+                    if (top == 0) {
+                        bw.write(-1 + "\n");
+                    } else {
+                        bw.write(stack[top - 1] + "\n");
+                    }
+                    break;
             }
         }
-
+        bw.flush();
         bw.close();
-
     }
 }
