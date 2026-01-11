d1956dde25c4c4868c12bf8aba0b97ba15ab4bc4
YeongSu1000 <147939982+YeongSu1000@users.noreply.github.com>
2026-01-11 19:07:26 +0900

    [Silver IV] Title: 요세푸스 문제 0, Time: 120 ms, Memory: 14912 KB -BaekjoonHub

--- 백준/Silver/11866. 요세푸스 문제 0/요세푸스 문제 0.java
+++ 백준/Silver/11866. 요세푸스 문제 0/요세푸스 문제 0.java
@@ -2,80 +2,54 @@ import java.io.*;
 import java.util.StringTokenizer;
 
 public class Main {
-    public static int[] arrayCopy(int arr[], int k, int size) {
-        int[] array = new int[size - 1];
-        int c = 0;
-        for (int i = 0; i < array.length; i++) {
-            if (k + i >= size) {
-                array[i] = arr[c];
-                c++;
-            } else {
-                array[i] = arr[k + i];
-            }
-        }
-        return array;
-    }
-
-    public static int[] arrayCopy2(int arr[], int k, int size) { // 7 1 2 3 4
-        int[] array = new int[size - 1]; // 4
-        int c = 0;
-        int a = 0;
-        int index;
-        index = k - array.length - 1; // 10 - 2 - 1
-        index--;
-        while (index >= arr.length) {
-            //index = k % arr.length;
-            index -= arr.length;
-        }
-
-
-        for (int i = 0; i < array.length; i++) {  // 4
-            if (i == index) { // 0 == 6 - 5 - 1
-
-            }
-            if (array.length - index > i) { // 인덱스보다 큰 3 - 2 > i
-                array[i] = arr[index + i + 1]; // 0 = 2 + 0 + 1
-
-            } else {
-                array[i] = arr[c];
-                c++;
-            }
-        }
-        return array;
-    }
-
     public static void main(String[] args) throws IOException {
+
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        // n,k
         StringTokenizer st = new StringTokenizer(br.readLine());
         int n = Integer.parseInt(st.nextToken());
         int k = Integer.parseInt(st.nextToken());
-        int[] array = new int[n];
-        for (int i = 1; i <= n; i++) {
-            array[i - 1] = i;
+
+        int[] list = new int[n];
+        for (int i = 0; i < n; i++) {
+            list[i] = i + 1;
         }
 
-        int i = 0;
+        int first = 0;
+        int last = n - 1;
+        int size = n;
         bw.write("<");
-        while (array.length >= k && array.length != 1) {
-            bw.write(array[k - 1] + ", ");
-            array = arrayCopy(array, k, array.length);
-        }
+        while (size > 1) {
 
-        int index;
-        while (array.length < k && array.length > 1) {
-            index = k - array.length - 1;
-            while (index >= array.length) {
-                index -= array.length;
+            for (int i = 0; i < k - 1; i++) {
+                last++;
+                if (last > n - 1) {
+                    last = 0;
+                }
+
+
+                list[last] = list[first];
+                first++;
+
+                if(first > n - 1){
+                    first = 0;
+                }
             }
-            //index -= 1; 1 2 3 4 5 - 4
 
-            bw.write(array[index] + ", ");
-            array = arrayCopy2(array, k, array.length);
+            // pop
+            bw.write(list[first] + ", ");
+            first++;
+            if(first > n - 1){
+                first = 0;
+            }
+
+            size--;
         }
-        bw.write(array[0] + ">");
+        bw.write(list[last] + ">");
 
 
+        bw.flush();
         bw.close();
     }
 }
