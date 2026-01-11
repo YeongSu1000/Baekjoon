24660cd0a372930352a2b01cbda1568071ee6782
YeongSu1000 <147939982+YeongSu1000@users.noreply.github.com>
2026-01-11 18:05:29 +0900

    [Silver IV] Title: 큐, Time: 172 ms, Memory: 19492 KB -BaekjoonHub

--- 백준/Silver/10845. 큐/큐.java
+++ 백준/Silver/10845. 큐/큐.java
@@ -1,73 +1,63 @@
 import java.io.*;
+import java.util.StringTokenizer;
 
 public class Main {
-    static int first = 0;
-    static int last = 0;
-
-    public static void push(int x, int[] que) {
-        que[last] = x;
-        last++;
-    }
-
-    public static int pop(int[] que) {
-        int r = -1;
-        if(last - first == 0){
-            return r;
-        }else {
-            r = que[first];
-            que[first] = 0;
-            first++;
-            return r;
-        }
-    }
-
-    public static int size() {
-        return last - first;
-    }
-
-    public static int empty() {
-        if (last - first > 0){
-            return 0;
-        } else return 1;
-    }
-
-    public static int front(int[] que) {
-        if(last - first == 0){
-            return -1;
-        }else return que[first];
-        
-    }
-
-    public static int back(int[] que){
-        if(last - first == 0){
-            return -1;
-        }else return que[last - 1];
-    }
-
     public static void main(String[] args) throws IOException {
+
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
         int n = Integer.parseInt(br.readLine());
-        int[] que = new int[n];
+
+        int first = 0;
+        int last = 0;
+
+        int[] list = new int[n];
+
         for (int i = 0; i < n; i++) {
-            String s = br.readLine();
-            if(s.contains("push")){
-                int x = Integer.parseInt(s.substring(s.indexOf(" ")+1));
-                push(x, que);
-            }else if(s.equals("pop")){
-                bw.write(pop(que) + "\n");
-            }else if(s.equals("size")){
-                bw.write(size() + "\n");
-            }else if(s.equals("empty")){
-                bw.write(empty() + "\n");
-            }else if(s.equals("front")){
-                bw.write(front(que) + "\n");
-            }else if(s.equals("back")){
-                bw.write(back(que) + "\n");
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            String s = st.nextToken();
+            switch (s) {
+                case "push":
+                    int add = Integer.parseInt(st.nextToken());
+                    list[last] = add;
+                    last++;
+
+                    break;
+                case "pop":
+                    if (first == last) {
+                        bw.write("-1\n");
+                    } else {
+                        bw.write(list[first] + "\n");
+                        first++;
+                    }
+
+                    break;
+                case "size":
+                    bw.write(last - first + "\n");
+
+                    break;
+                case "empty":
+                    if (last - first == 0) {
+                        bw.write("1\n");
+                    } else bw.write("0\n");
+
+                    break;
+                case "front":
+                    if (last - first == 0) {
+                        bw.write("-1\n");
+                    } else bw.write(list[first] + "\n");
+                    
+                    break;
+                case "back":
+                    if (last - first == 0) {
+                        bw.write("-1\n");
+                    } else bw.write(list[last - 1] + "\n");
+                    
+                    break;
             }
         }
-
+        bw.flush();
         bw.close();
-
     }
 }
